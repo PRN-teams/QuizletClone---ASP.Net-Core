79853818968dbeaaa219149cad0d971574abe651
meof-coding <tranthe150186@fpt.edu.vn>
2021-12-05 21:10:14 +0700

    Payment (80%) 🐳

--- WebApplication1/Controllers/PaymentController.cs
+++ WebApplication1/Controllers/PaymentController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Razorpay.Api;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using WebApplication1.Models;
 
@@ -13,7 +17,7 @@ namespace WebApplication1.Controllers
 {
     public class PaymentController : Controller
     {
-        
+
         private readonly ILogger<PaymentController> _logger;
         private readonly DBQuizSharpContext _context;
         private readonly IConfiguration _config;
@@ -35,9 +39,11 @@ namespace WebApplication1.Controllers
                 Description = $"Transaction of customer {@TempData.Peek("username")} in {DateTime.Now}",
                 UId = Convert.ToInt32(@TempData.Peek("uid")),
                 Date = DateTime.Now,
+                U = (from u in _context.User where u.Id == Convert.ToInt32(TempData.Peek("uid")) select u).Single()
             };
             bill.OrderId = CreateOrder(bill);
             ViewBag.Key = _key;
+            TempData["Bill"] = JsonConvert.SerializeObject(bill);
             return View(bill);
         }
 
@@ -53,13 +59,71 @@ namespace WebApplication1.Controllers
                 options.Add("receipt", $"order_rcptid_{@TempData.Peek("uid")}");
                 options.Add("currency", bill.Currency);
                 Order order = client.Order.Create(options);
-                var orderID = order.Attributes["id"].ToString();    
+                var orderID = order.Attributes["id"].ToString();
                 return orderID;
             }
             catch (Exception)
             {
                 return null;
             }
+        }
+
+        public ActionResult AfterPayment()
+        {
+            var paymentStatus = Request.Form["paymentStatus"].ToString();
+            if (paymentStatus.Equals("Fail"))
+            {
+                Bill bill = (Bill)TempData["Bill"];
+                bill.Status = paymentStatus;
+                _context.Add(bill);
+                return View();
+            }
+            var paymentID = Request.Form["paymentId"].ToString();
+            var orderID = Request.Form["orderID"].ToString();
+            var signature = Request.Form["signature"].ToString();
+            var validSignature = CompareSignatures(orderID, paymentID, signature);
+            if (validSignature)
+            {
+                Bill bill = JsonConvert.DeserializeObject<Bill>(TempData["Bill"].ToString());
+                bill.Status = paymentStatus;
+                _context.Add(bill);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        private bool CompareSignatures(string orderid, string paymentid,string signature)
+        {
+            var text = orderid + "|" + paymentid;
+            var _keysecret = _config["Authentication:RazorPay:KeySecret"];
+            var generatedSignature = CalculateSHA256(text,_keysecret);
+            if (generatedSignature.Equals(signature))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private string CalculateSHA256(string text, string secret)
+        {
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            Byte[] textBytes = encoding.GetBytes(text);
+            Byte[] keyBytes = encoding.GetBytes(secret);
+
+            Byte[] hashBytes;
+
+            using (HMACSHA256 hash = new HMACSHA256(keyBytes))
+                hashBytes = hash.ComputeHash(textBytes);
+
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+           
         }
     }
 }
